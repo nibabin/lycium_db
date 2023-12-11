7e6b61958642dcca402410050c855a2c510040de
Ryan Cunningham <rcunningham24@amherst.edu>
2023-12-10 21:01:48 -0500

    slightly improved specimendata formatting

--- client/src/pages/SpecimenData.jsx
+++ client/src/pages/SpecimenData.jsx
@@ -1,58 +1,55 @@
-import React from 'react';
-import SpecimenAPI from '../../services/SpecimenAPI';
-import { useState, useEffect } from 'react';
-import { Button } from '@chakra-ui/react'
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import SpecimenAPI from '../../services/SpecimenAPI';
+import { VStack, Text } from '@chakra-ui/react';
 
+export default function SpecimenData() {
+  const { id } = useParams();
+  const [specimen, setSpecimen] = useState(null);
 
-export default function SpecimenData(){
-    const { id } = useParams();
-    const [specimen, setSpecimen] = useState([]);
+  useEffect(() => {
+    const fetchSpecimen = async () => {
+      try {
+        const data = await SpecimenAPI.getSpecimenById(id);
+        setSpecimen(data);
+      } catch (error) {
+        console.error('Error catching species:', error);
+      }
+    };
 
-    useEffect(()=>{
-        fetchSpecimen()
-    }, [SpecimenAPI])
+    fetchSpecimen();
+  }, [id]);
 
+  const monthNames = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
 
+  if (!specimen) {
+    return <p>Loading...</p>;
+  }
 
-    const fetchSpecimen = async() =>{
-        try{
-            const data = await SpecimenAPI.getSpecimenById(id)
-            setSpecimen(data)
-
-        }catch(error){
-            console.error('Error catching species:', error)
-        }
-    }
-
-    const monthNames = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
-      ];
-
-
-    return(
-        <div className=''>
-            <p><strong>Genus</strong>: {specimen.genus}</p>
-            <p><strong>Species</strong>: {specimen.species}</p>
-            <p><strong>Extraction Number</strong>: {specimen.extraction_number}</p>
-            <p><strong>Extraction Date</strong>: {specimen.extraction_date}</p>
-            <p><strong>Field ID</strong>: {specimen.field_pop_id}</p>
-            <p><strong>Greenhouse</strong>: {specimen.greenhouse}</p>
-            <p><strong>Voucher</strong>: {specimen.voucher_specimen}</p>
-            <p><strong>Collection Date:</strong> {new Date(specimen.collection_date).getDate()} {monthNames[new Date(specimen.collection_date).getMonth()]} {new Date(specimen.collection_date).getFullYear()}</p>
-            <p><strong>Provenance</strong>: {specimen.provenance}</p>
-            <p><strong>Country</strong>: {specimen.country}</p>
-            <p><strong>State Provenance</strong>: {specimen.state_province}</p>
-            <p><strong>Specific Local</strong>: {specimen.specific_locality}</p>
-            <p><strong>Latitude</strong>: {specimen.latitude}</p>
-            <p><strong>Longitude</strong>: {specimen.longitude}</p>
-            <p><strong>Notes</strong>: {specimen.notes}</p>
-            <p><strong>Material</strong>: {specimen.material}</p>
-            <p><strong>Nanodrop Concentration</strong>: {specimen.nanodrop_concentration}</p>
-            <p><strong>Nanodrop Ratio</strong>: {specimen.nanodrop_ratio}</p>
-            <p><strong>Published</strong>: {specimen.published}</p>
-        </div>
-    )
+  return (
+    <VStack spacing={4} align='stretch'>
+      <Text><strong>Genus</strong>: {specimen.genus}</Text>
+      <Text><strong>Species</strong>: {specimen.species}</Text>
+      <Text><strong>Extraction Number</strong>: {specimen.extraction_number}</Text>
+      <Text><strong>Extraction Date</strong>: {specimen.extraction_date}</Text>
+      <Text><strong>Field ID</strong>: {specimen.field_pop_id}</Text>
+      <Text><strong>Greenhouse</strong>: {specimen.greenhouse}</Text>
+      <Text><strong>Voucher</strong>: {specimen.voucher_specimen}</Text>
+      <Text><strong>Collection Date:</strong> {new Date(specimen.collection_date).getDate()} {monthNames[new Date(specimen.collection_date).getMonth()]} {new Date(specimen.collection_date).getFullYear()}</Text>
+      <Text><strong>Provenance</strong>: {specimen.provenance}</Text>
+      <Text><strong>Country</strong>: {specimen.country}</Text>
+      <Text><strong>State Provenance</strong>: {specimen.state_province}</Text>
+      <Text><strong>Specific Local</strong>: {specimen.specific_locality}</Text>
+      <Text><strong>Latitude</strong>: {specimen.latitude}</Text>
+      <Text><strong>Longitude</strong>: {specimen.longitude}</Text>
+      <Text><strong>Notes</strong>: {specimen.notes}</Text>
+      <Text><strong>Material</strong>: {specimen.material}</Text>
+      <Text><strong>Nanodrop Concentration</strong>: {specimen.nanodrop_concentration}</Text>
+      <Text><strong>Nanodrop Ratio</strong>: {specimen.nanodrop_ratio}</Text>
+      <Text><strong>Published</strong>: {specimen.published}</Text>
+    </VStack>
+  );
 }
