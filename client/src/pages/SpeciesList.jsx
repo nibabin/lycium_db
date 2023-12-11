2438bc93fdb9d87322e23ec929769038fe925ac4
Claudiu Babin <cbabin@gitlab-ncsa.ubisoft.org>
2023-12-10 20:46:19 -0500

    fixed some issues with Custom View

--- client/src/pages/SpeciesList.jsx
+++ client/src/pages/SpeciesList.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import GeneticsAPI from '../../services/GeneticsAPI';
 import { useState, useEffect } from 'react';
-import { Button } from '@chakra-ui/react'
+import { Button, Spinner } from '@chakra-ui/react'
 import '../css/SpeciesList.css'
 import { useNavigate } from 'react-router-dom';
 
 
 export default function SpeciesList(){
     const [species, setSpecies] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
 
     useEffect(()=>{
         fetchSpecies()
@@ -17,9 +19,11 @@ export default function SpeciesList(){
     const fetchSpecies = async() =>{
         try{
             const data = await GeneticsAPI.getAllGenetics({})
-            setSpecies(data)
+            setSpecies(data.filter(s => (s.species || s.genus)))
+            setIsLoading(false)
         }catch(error){
             console.error('Error catching species:', error)
+            setIsLoading(false)
         }
     }
 
@@ -32,15 +36,20 @@ export default function SpeciesList(){
 
     return(
         <div className='species-container'>
-            {species.length === 0 && <p>No species found.</p>}
-            {species.map((s, idx) => {
-                return (
+            {isLoading && (
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Spinner className="spinner" size="xl" />
+                </div>
+            )}
+            {!isLoading && species.length === 0 && <p>No species found.</p>}
+            {!isLoading && species.map((s, idx) => {
+                return ( 
                     <div key={idx} className='species-item'>
                         <p><strong>Species</strong>: {s.species}</p>
                         <p><strong>Genus</strong>: {s.genus}</p>
                         <Button className="species-button" onClick={() => handleViewSpecimenClick(s.genetics_id)} colorScheme='teal'>View Specimen</Button>
                     </div>
-                );
+                ) 
             })}
 
         </div>
