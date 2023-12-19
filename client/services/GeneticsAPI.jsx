34ee921d8cfc796e6f62dd336cd6094d0d7da973
Charlie <charlesclary0@gmail.com>
2023-12-18 21:03:30 -0600

    Switched frontend to use backend server

--- client/services/GeneticsAPI.jsx
+++ client/services/GeneticsAPI.jsx
@@ -1,6 +1,6 @@
 import { request } from '../utilities/api'
 
-const geneticsURL = 'http://localhost:3000/genetics'
+const geneticsURL = 'http://cosc-257-node09.cs.amherst.edu:3000/genetics'
 
 const getGeneticsByParams = (params) => request('GET', geneticsURL, params)
 const addGenetics = (genetics) => request('POST', geneticsURL, genetics)
